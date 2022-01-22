10e3e5d1534d75bd57a4e30b96037e65d702941e
DhrumilP <dhrumil172@gmail.com>
2022-01-22 15:16:53 -0500

    Wallet connect functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 // Constants
@@ -6,10 +6,13 @@ const WEBSITE_NAME = 'Dhrumil Pandya';
 const WEBSITE_LINK = 'https://dhrumilpandya.com/';
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
    */
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -18,6 +21,12 @@ const App = () => {
       if (solana) {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
         }
       } else {
         alert('Install phantom! ');
@@ -26,6 +35,21 @@ const App = () => {
       console.error(error);
     }
   };
+
+  const connectWallet = async () => {};
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className='cta-button connect-wallet-button'
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
 
   /*
    * When our component first mounts, let's check to see if we have a connected
@@ -41,12 +65,13 @@ const App = () => {
 
   return (
     <div className='App'>
-      <div className='container'>
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className='header-container'>
           <p className='header'>🚀 Memories Portal</p>
           <p className='sub-text'>
             View your memories collection in the metaverse 🐼
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className='footer-container'>
           <a
